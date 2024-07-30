64d67824231621f1e6959eeb29b223da031efc59
Mayitzin <mayitzin@gmail.com>
2024-07-30 14:33:24 +0200

    Remove function _set_axis_equal(). Matplotlib introduced method set_aspect('equal') in version 3.6

--- ezview/plot.py
+++ ezview/plot.py
@@ -12,34 +12,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from .colors import COLORS
 
-def _set_axes_equal(ax):
-    """
-    Make axes of 3D plot ``ax`` have equal scale along its 3 axes.
-
-    Parameters
-    ----------
-    ax : axes.SubplotBase subclass of Axes (or a subclass of Axes)
-        Matplotlib axis, e.g., as output from plt.gca().
-    """
-    x_limits = ax.get_xlim3d()
-    y_limits = ax.get_ylim3d()
-    z_limits = ax.get_zlim3d()
-    # Compute ranges along each axis
-    x_range = abs(x_limits[1] - x_limits[0])
-    x_middle = np.mean(x_limits)
-    y_range = abs(y_limits[1] - y_limits[0])
-    y_middle = np.mean(y_limits)
-    z_range = abs(z_limits[1] - z_limits[0])
-    z_middle = np.mean(z_limits)
-
-    # The plot bounding box is a sphere in the sense of the infinity
-    # norm, hence I call half the max range the plot radius.
-    plot_radius = 0.5*max([x_range, y_range, z_range])
-
-    ax.set_xlim3d([x_middle - plot_radius, x_middle + plot_radius])
-    ax.set_ylim3d([y_middle - plot_radius, y_middle + plot_radius])
-    ax.set_zlim3d([z_middle - plot_radius, z_middle + plot_radius])
-
 def plot_data(*data, **kw):
     """
     Plot data with custom formatting.
@@ -197,7 +169,7 @@ def plot3(data, style='line', sphere=None) -> None:
         z = np.cos(v) * sz + cz
         # x, y, z = projection @ np.c_[x, y, z].T
         ax.plot_wireframe(x, y, z, color=COLORS[2], lw=0.3)
-    _set_axes_equal(ax)
+    ax.set_aspect('equal')      # Added in matplotlib 3.6
     plt.tight_layout()
     plt.show()
 
@@ -272,7 +244,7 @@ def plot_kinematics(data, ref_data: np.ndarray = None, tip: np.ndarray = None, s
         ax.set_xlabel('X')
         ax.set_ylabel('Y')
         ax.set_zlabel('Z')
-        _set_axes_equal(ax)
+        ax.set_aspect('equal')      # Added in matplotlib 3.6
     plt.tight_layout()
     plt.show()
 
