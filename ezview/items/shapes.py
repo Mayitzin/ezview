4377ae68e652fb7b6ba3ae668a9c772099b6d6f0
Mayitzin <mayitzin@gmail.com>
2024-07-31 10:32:59 +0200

    Add function to create coordinates of 3d frames.

--- ezview/items/shapes.py
+++ ezview/items/shapes.py
@@ -15,16 +15,16 @@ def ellipsoid(center: np.ndarray = None, axes: np.ndarray = None, num_points: in
     ellipsoid is defined by the equation:
 
     .. math::
-    
-            \\frac{x^2}{a^2} + \\frac{y^2}{b^2} + \\frac{z^2}{c^2} = 1
+
+        \\frac{x^2}{a^2} + \\frac{y^2}{b^2} + \\frac{z^2}{c^2} = 1
 
     The mesh is built using the parametric equations:
 
     .. math::
 
-            x = a \\cos(u) \\sin(v) + x_0
-            y = b \\sin(u) \\sin(v) + y_0
-            z = c \\cos(v) + z_0
+        x = a \\cos(u) \\sin(v) + x_0
+        y = b \\sin(u) \\sin(v) + y_0
+        z = c \\cos(v) + z_0
 
     The mesh is built using the numpy.mgrid function in the range
     :math:`u \\in [0, 2\\pi]` and :math:`v \\in [0, \\pi]`. The default number
@@ -48,12 +48,14 @@ def ellipsoid(center: np.ndarray = None, axes: np.ndarray = None, num_points: in
     tuple
         Tuple with the mesh of the ellipsoid in the form (x, y, z).
 
-    Examples
-    --------
-    >>> x, y, z = ellipsoid()
-    (array([1., 1., 1., ..., 1., 1., 1.]),
-     array([0., 0., 0., ..., 0., 0., 0.]),
-     array([0., 0., 0., ..., 0., 0., 0.]))
+    Example
+    -------
+    >>> x, y, z = ezview.shapes.ellipsoid()
+    >>> x.shape, y.shape, z.shape
+    ((20, 20), (20, 20), (20, 20))
+    >>> x, y, z = ezview.shapes.ellipsoid(center=[1, 2, 3], axes=[2, 3, 4], num_points=20)
+    >>> x.shape, y.shape, z.shape
+    ((20, 20), (20, 20), (20, 20))
 
     """
     if center is None:
@@ -72,3 +74,34 @@ def ellipsoid(center: np.ndarray = None, axes: np.ndarray = None, num_points: in
     y = sy * np.sin(u)*np.sin(v) + cy
     z = sz * np.cos(v) + cz
     return x, y, z
+
+def frame(dcm: np.ndarray = None, position: np.ndarray = None, scale: float = 1.0) -> list:
+    """
+    Return the coordinates of an orthogonal frame.
+
+    It returns the coordinates of an orthogonal frame given its direction
+    cosine matrix (DCM) and position. The DCM is a 3-by-3 array with the
+    frame's axes as columns.
+
+    The position is a 3-element array with the frame's origin.
+
+    The scale parameter is used to scale the frame's axes.
+
+    If the DCM is None, it returns the identity matrix. If the position is
+    None, it builds the frame from the origin.
+
+    Parameters
+    ----------
+    dcm : numpy.ndarray, optional
+        3-by-3 array with the frame's axes. Default is the identity matrix.
+    position : numpy.ndarray, optional
+        3-element array with the frame's origin. Default is [0, 0, 0].
+    scale : float, optional
+        Scale factor for the frame's axes. Default is 1.0.
+
+    """
+    f_coords = []
+    for column_index in range(3):
+        axis_end = dcm[:, column_index]*scale + position
+        f_coords.append(np.c_[position, axis_end])
+    return f_coords
